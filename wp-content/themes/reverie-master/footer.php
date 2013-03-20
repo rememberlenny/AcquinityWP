0bc74e7b344cb5fda61170260a6de0d7bbe5d08b
lkbgift <lkbgift@gmail.com>
2013-03-20 15:58:29 -0400

    set up footer.php and header.php

--- wp-content/themes/reverie-master/footer.php
+++ wp-content/themes/reverie-master/footer.php
@@ -1,19 +1,22 @@
-</section><!-- Container End -->
+</section><!--  End -->
+
+</div><!-- Container End -->
 
 <div class="row full-width">
 	<?php dynamic_sidebar("Footer"); ?>
 </div>
 
-<footer class="row full-width" role="contentinfo">
-	<div class="small-12 large-4 columns">
-		<p>&copy; <?php echo date('Y'); ?>. Crafted on <a href="http://themefortress.com/reverie/" rel="nofollow" title="Reverie Framework">Reverie</a>.</p>
-	</div>
 	
-	<div class="small-12 large-8 columns">
-		<?php wp_nav_menu(array('theme_location' => 'utility', 'container' => false, 'menu_class' => 'inline-list right')); ?>
+<footer class="row full-width" role="contentinfo">
+	<div class="footer-full twelve">
+		<div class="row footer">
+			<div class="column">
+				<?php wp_nav_menu(array('theme_location' => 'utility', 'container' => 'nav', 'container_class' => '', 'menu_class' => 'inline-list small-block-grid-2')); ?>
+			<p>&copy; <?php echo date('Y'); ?> <a href="http://http://acquinityinteractive.com" rel="nofollow" title="Acquinity Interactive">Acquinity Interactive - All Rights Reserved</a>.</p>
+			</div>
+		</div>
 	</div>
 </footer>
-
 <?php wp_footer(); ?>
 
 <script>
