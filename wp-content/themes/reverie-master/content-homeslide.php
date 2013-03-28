129262338aad33cee8fc3065f8be32588884a576
lkbgift <lkbgift@gmail.com>
2013-03-28 15:11:48 -0400

    created homepage integration with blog and scucess stories loop

---
+++ wp-content/themes/reverie-master/content-homeslide.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * The default template for displaying content. Used for both single and index/archive/search.
+ *
+ * @subpackage Reverie
+ * @since Reverie 4.0
+ */
+?>
+	
+ <li class="active text-left article-title" data-orbit-slide="headline-1">
+   <div id="post-<?php the_ID(); ?>">
+      <div class="large-3 small-3 mkup column mn93">
+        <div class="hide-for-small image-wrapper circlewrapper radius" >
+          <?php if (has_post_thumbnail( $post->ID ) ): ?>
+            <?php $image = wp_get_attachment_image_src( the_post_thumbnail( 'thumbnail' ), 'single-post-thumbnail' ); ?>
+          <?php endif; ?>
+        </div>
+      </div>
+      <div class="large-9 small-12 column pl98">
+       <header>
+        <h5>
+          <a href="<?php the_permalink(); ?>" data-orbit-slide="headline-2" class="secondary">
+            <?php the_title(); ?>
+          </a>
+        </h5> 
+    	</header>
+    		<p>
+          <?php
+            $excerpt = get_the_excerpt();
+            echo string_limit_words($excerpt,25);
+          ?>... 
+          <a href="<?php the_permalink(); ?>">Read More ›</a>
+        </p>  
+    	<footer>
+    		<?php $tag = get_the_tags(); if (!$tag) { } else { ?><p><?php the_tags(); ?></p><?php } ?>
+    	</footer>
+    </div>
+  </div>
+</li> 
---
+++ wp-content/themes/reverie-master/content-newpost.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * The default template for displaying content. Used for both single and index/archive/search.
+ *
+ * @subpackage Reverie
+ * @since Reverie 4.0
+ */
+?>
+	
+ <li class="active text-left article-title" data-orbit-slide="headline-1">
+  <ul style="margin-bottom: 0.4em;">
+    <li>
+      <div class="image-wrapper radius" >
+        <?php if (has_post_thumbnail( $post->ID ) ): ?>
+          <?php $image = wp_get_attachment_image_src( the_post_thumbnail( 'thumbnail' ), 'single-post-thumbnail' ); ?>
+        <?php endif; ?>
+      </div>
+    </li>
+  </ul>
+  <h5 class="article-title-line" style="line-height:1.2;">
+    <a href="<?php the_permalink(); ?>" class="secondary">
+      <?php the_title(); ?>
+    </a>
+  </h5>
+  <p class=" text-left">
+    <?php
+      $excerpt = get_the_excerpt();
+      echo string_limit_words($excerpt,10);
+    ?>...
+    <a class="svbt-line" href="<?php the_permalink(); ?>">Read Article ›</a>
+  </p>  
+</li> 
