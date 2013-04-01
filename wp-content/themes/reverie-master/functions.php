f2c70262289636f1b574472f94add302999a48a2
lkbgift <lkbgift@gmail.com>
2013-04-01 17:12:41 -0400

    font-size changes and front page style

--- wp-content/themes/reverie-master/functions.php
+++ wp-content/themes/reverie-master/functions.php
@@ -128,7 +128,7 @@ function page_bottom_box(){
           ?> 
           </h4>
         </div>
-        <div class="large-6 column">              
+        <div class="large-6 fl">              
          <p>
             <?php 
               the_field('page_bottom_statistic_short_desc'); 
