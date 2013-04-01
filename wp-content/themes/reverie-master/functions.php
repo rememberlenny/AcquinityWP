bdad5c100ce6dfc5a31a15349154218d9e8130ec
lkbgift <lkbgift@gmail.com>
2013-04-01 16:18:10 -0400

    used convoluted is_home

--- wp-content/themes/reverie-master/functions.php
+++ wp-content/themes/reverie-master/functions.php
@@ -230,7 +230,7 @@ function lkbg_head_style(){
             background: url("<?php echo $image[0]; ?>") center center no-repeat #FAF8F6 !important;
           }
       <?php } ?> 
-    <?php elseif (is_page('news') ): ?>
+    <?php elseif (is_home() ): ?>
       div.masthead-photo {
         background: url("<?php the_field('blog_top_header', 'option') ?>
           ") center center no-repeat #FAF8F6;
