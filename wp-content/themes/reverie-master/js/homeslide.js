317b8eb5c1d1b64c9eb6f05a7e63fed40e4584b9
lkbgift <lkbgift@gmail.com>
2013-04-02 09:49:22 -0400

    fixing orbit slider on frontpage by changing jquery activation

--- wp-content/themes/reverie-master/js/homeslide.js
+++ wp-content/themes/reverie-master/js/homeslide.js
@@ -1,7 +1,7 @@
 $(document).ready(function()    {
-  $('#brandslider').foundation('orbit', {  stack_on_small: false, animation_speed: 750, bullets: false, bullets: true, }, function (response) {
+  $('#brandslider').foundation('orbit', {  timer_speed: 10000,stack_on_small: false, animation_speed: 750, bullets: false, bullets: true, }, function (response) {
   });
-  $('#featsucc').foundation('orbit', {  timer_speed: 0, stack_on_small: false, animation_speed: 750, bullets: false, bullets: true, }, function (response) {
+  $('#featsucc').foundation('orbit', { stack_on_small: false, animation_speed: 750, bullets: false, bullets: true, }, function (response) {
     $('#featsucc').find('.orbit-timer span').click();
   });
 });
