5496d32663928ac53795ec0a3f9f9b79a587e73a
lkbgift <lkbgift@gmail.com>
2013-04-02 10:54:29 -0400

    making columns of archive page

--- wp-content/themes/reverie-master/content-archive.php
+++ wp-content/themes/reverie-master/content-archive.php
@@ -10,20 +10,22 @@
 
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<div class="thumb-wrapper circlewrapper">
-	<?php if (has_post_thumbnail( $post->ID ) ): ?>
-    <?php $image = wp_get_attachment_image_src( the_post_thumbnail( 'thumbnail' ), 'single-post-thumbnail' ); ?>
-  <?php endif; ?>
+  <div class="large-4 column">
+  	<div class="thumb-wrapper circlewrapper">
+  	<?php if (has_post_thumbnail( $post->ID ) ): ?>
+      <?php $image = wp_get_attachment_image_src( the_post_thumbnail( 'thumbnail' ), 'single-post-thumbnail' ); ?>
+    <?php endif; ?>
+    </div>
   </div>
-	<header>
-		<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-	</header>
-  <div class="content-block"> 
-		<?php the_excerpt(''); ?>
-		<a class="svbt-line d-inline" href="<?php the_permalink(); ?>"> Read More</a>
+  <div class="content-block large-8 column"> 
+    <header>
+      <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    </header>
+  		<?php the_excerpt(''); ?>
+  		<a class="svbt-line d-inline" href="<?php the_permalink(); ?>"> Read More</a>
+    <footer>
+      <?php $tag = get_the_tags(); if (!$tag) { } else { ?><p><?php the_tags(); ?></p><?php } ?>
+    </footer>
   </div>  
-	<footer>
-		<?php $tag = get_the_tags(); if (!$tag) { } else { ?><p><?php the_tags(); ?></p><?php } ?>
-	</footer>
 	<hr />
 </article>
