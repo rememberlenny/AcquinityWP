85548cb10098e133a0c66ce2c2534dbce383b45d
lkbgift <lkbgift@gmail.com>
2013-04-05 14:33:33 -0400

    fixing double underscore

--- wp-content/themes/reverie-master/archive.php
+++ wp-content/themes/reverie-master/archive.php
@@ -7,25 +7,17 @@ get_header(); ?>
 <style type="text/css">
   <?php
    if       (is_category('press-release') ):    
-      $url = get_field('blog-category__press_release_image', 'option');
-      $hero_text_content = get_field('blog-category__press_release_text', 'option');
-      heroBackgroundImage($url);  
-      header_text_block_template ($hero_text_content); 
+      $url = get_field('blog-category_press_release_image', 'option');
+      heroBackgroundImage($url);   
     elseif  (is_category('event-appearances') ):    
       $url = get_field('blog-category_event_appearance_image', 'option');
-      $hero_text_content = get_field('blog-category_event_appearance_text', 'option');
-       heroBackgroundImage($url);  
-       header_text_block_template ($hero_text_content);  
+       heroBackgroundImage($url);    
     elseif  (is_category('industry-news') ):     
       $url = get_field('blog-category_industry_news_image', 'option');
-      $hero_text_content = get_field('blog-category_industry_news_text', 'option');
       heroBackgroundImage($url);
-      header_text_block_template ($hero_text_content);
     elseif  (is_category('blog') ):     
       $url = get_field('blog-category_blog_image', 'option');
-      $hero_text_content = get_field('blog-category_blog_text', 'option');
       heroBackgroundImage($url);
-      header_text_block_template ($hero_text_content);
     else :   
       defaultNewsHeader(); 
     endif;
