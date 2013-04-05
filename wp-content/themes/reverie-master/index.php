77d9d34b2ae0646fc8ce2624a069378a07d5f9aa
lkbgift <lkbgift@gmail.com>
2013-04-05 13:52:38 -0400

    fixing the appropriate index.php file

--- wp-content/themes/reverie-master/index.php
+++ wp-content/themes/reverie-master/index.php
@@ -7,7 +7,14 @@ get_header(); ?>
 <?php if (has_post_thumbnail( $post->ID ) ): ?>
 <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
 
-<?php inlineCSScall(); ?>
+<?php if(!is_home()):
+    inlineCSScall(); 
+  else:
+    echo '<style>';
+    defaultNewsHeader(); 
+    echo '</style>';
+  endif;
+  ?>
 
 <?php endif; ?>
 
