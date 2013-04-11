4c2895e90434a6bedd80e601e357f96e5ada9301
lkbgift <lkbgift@gmail.com>
2013-04-11 15:02:18 -0400

    make sure the timers work correctly

--- wp-content/themes/reverie-master/includes/lkbg_brand_logo.php
+++ wp-content/themes/reverie-master/includes/lkbg_brand_logo.php
@@ -4,22 +4,10 @@ function brandBarLogoContent(){
 
 ?>
 
-<ul id="brand-logo-ul" data-orbit style="opacity:0;">
-  <li class="active brand-row" data-orbit-slide style="">
-    <div>
-      <ul class="large-block-grid-6 small-block-grid-4">
-        <li><img src="images/logos/netflix-logo.png" alt=""></li>
-        <li><img src="images/logos/sc-johnson-logo.png" alt=""></li>
-        <li><img src="images/logos/vonage-logo.png" alt=""></li>
-        <li><img src="images/logos/tigerdirect-logo.png" alt=""></li>
-        <li class="hide-for-small"><img src="images/logos/terracom-logo.png" alt=""></li>
-        <li class="hide-for-small"><img src="images/logos/travellers-logo.png" alt=""></li>
-      </ul>
-    </div> 
-  </li>
+<ul data-orbit >
   <li class="brand-row" data-orbit-slide>
     <div>
-      <ul class="large-block-grid-6 small-block-grid-4">
+      <ul class="large-block-grid-6 small-block-grid-4  brand-logo-ul" style="opacity:0;" >
         <li><img src="images/logos/disney-logo.png" alt=""></li>
         <li><img src="images/logos/kelloggs-logo.png" alt=""></li>
         <li><img src="images/logos/ebay-logo.png" alt=""></li>
@@ -31,7 +19,7 @@ function brandBarLogoContent(){
   </li>
   <li class="brand-row" data-orbit-slide>
     <div>
-      <ul class="large-block-grid-6 small-block-grid-4">
+      <ul class="large-block-grid-6 small-block-grid-4  brand-logo-ul"  style="opacity:0;">
         <li><img src="images/logos/groupon-logo.png" alt=""></li>
         <li><img src="images/logos/overstock-logo.png" alt=""></li>
         <li><img src="images/logos/jsp-logo.png" alt=""></li>
@@ -41,78 +29,19 @@ function brandBarLogoContent(){
       </ul>
     </div> 
   </li>
-    <li class="brand-row" data-orbit-slide>
+  <li class="brand-row" data-orbit-slide style="">
     <div>
       <ul class="large-block-grid-6 small-block-grid-4">
-        <li><img src="images/logos/netflix-logo.png" alt=""></li>
-        <li><img src="images/logos/sc-johnson-logo.png" alt=""></li>
-        <li><img src="images/logos/vonage-logo.png" alt=""></li>
-        <li><img src="images/logos/tigerdirect-logo.png" alt=""></li>
-        <li class="hide-for-small"><img src="images/logos/terracom-logo.png" alt=""></li>
-        <li class="hide-for-small"><img src="images/logos/travellers-logo.png" alt=""></li>
+        <li style="opacity:0;" id="brand-logo-li-1" ><img src="images/logos/netflix-logo.png" alt=""></li>
+        <li style="opacity:0;" id="brand-logo-li-2" ><img src="images/logos/sc-johnson-logo.png" alt=""></li>
+        <li style="opacity:0;" id="brand-logo-li-3" ><img src="images/logos/vonage-logo.png" alt=""></li>
+        <li style="opacity:0;" id="brand-logo-li-4" ><img src="images/logos/tigerdirect-logo.png" alt=""></li>
+        <li style="opacity:0;" id="brand-logo-li-5"  class="hide-for-small"><img src="images/logos/terracom-logo.png" alt=""></li>
+        <li style="opacity:0;" id="brand-logo-li-6" class="hide-for-small"><img src="images/logos/travellers-logo.png" alt=""></li>
       </ul>
     </div> 
   </li>
-  <li class="brand-row" data-orbit-slide>
-    <div>
-      <ul class="large-block-grid-6 small-block-grid-4">
-        <li><img src="images/logos/disney-logo.png" alt=""></li>
-        <li><img src="images/logos/kelloggs-logo.png" alt=""></li>
-        <li><img src="images/logos/ebay-logo.png" alt=""></li>
-        <li><img src="images/logos/aarp-logo.png" alt=""></li>
-        <li class="hide-for-small"><img src="images/logos/eharmony-logo.png" alt=""></li>
-        <li class="hide-for-small"><img src="images/logos/travelocity-logo.png" alt=""></li>
-      </ul>
-    </div> 
-  </li>
-  <li class="brand-row" data-orbit-slide>
-    <div>
-      <ul class="large-block-grid-6 small-block-grid-4">
-        <li><img src="images/logos/groupon-logo.png" alt=""></li>
-        <li><img src="images/logos/overstock-logo.png" alt=""></li>
-        <li><img src="images/logos/jsp-logo.png" alt=""></li>
-        <li><img src="images/logos/shoedazzle.png" alt=""></li>
-        <li class="hide-for-small"><img src="images/logos/achievecard-logo.png" alt=""></li>
-        <li class="hide-for-small"><img src="images/logos/educationdynamics-logo.png" alt=""></li>
-      </ul>
-    </div> 
-  </li>
-    <li class=" brand-row" data-orbit-slide>
-    <div>
-      <ul class="large-block-grid-6 small-block-grid-4">
-        <li><img src="images/logos/netflix-logo.png" alt=""></li>
-        <li><img src="images/logos/sc-johnson-logo.png" alt=""></li>
-        <li><img src="images/logos/vonage-logo.png" alt=""></li>
-        <li><img src="images/logos/tigerdirect-logo.png" alt=""></li>
-        <li class="hide-for-small"><img src="images/logos/terracom-logo.png" alt=""></li>
-        <li class="hide-for-small"><img src="images/logos/travellers-logo.png" alt=""></li>
-      </ul>
-    </div> 
-  </li>
-  <li class="brand-row" data-orbit-slide>
-    <div>
-      <ul class="large-block-grid-6 small-block-grid-4">
-        <li><img src="images/logos/disney-logo.png" alt=""></li>
-        <li><img src="images/logos/kelloggs-logo.png" alt=""></li>
-        <li><img src="images/logos/ebay-logo.png" alt=""></li>
-        <li><img src="images/logos/aarp-logo.png" alt=""></li>
-        <li class="hide-for-small"><img src="images/logos/eharmony-logo.png" alt=""></li>
-        <li class="hide-for-small"><img src="images/logos/travelocity-logo.png" alt=""></li>
-      </ul>
-    </div> 
-  </li>
-  <li class="brand-row" data-orbit-slide>
-    <div>
-      <ul class="large-block-grid-6 small-block-grid-4">
-        <li><img src="images/logos/groupon-logo.png" alt=""></li>
-        <li><img src="images/logos/overstock-logo.png" alt=""></li>
-        <li><img src="images/logos/jsp-logo.png" alt=""></li>
-        <li><img src="images/logos/shoedazzle.png" alt=""></li>
-        <li class="hide-for-small"><img src="images/logos/achievecard-logo.png" alt=""></li>
-        <li class="hide-for-small"><img src="images/logos/educationdynamics-logo.png" alt=""></li>
-      </ul>
-    </div> 
-  </li>
+
 </ul>
 <?php
 
@@ -123,7 +52,7 @@ function brandBarHome(){
 <div class="content brandwrap row twelve">
   <div class="main large-12 columns" >
     <div class="brand-bar clearboth" >
-      <div class="brand-bar-extension" >
+      <div  id="brand-logo-case" class="brand-bar-extension" >
         <div id="brandslider" class="brand-bar-content brand-color-back">
           <?php brandBarLogoContent(); ?>
         </div>
--- wp-content/themes/reverie-master/js/app.js
+++ wp-content/themes/reverie-master/js/app.js
@@ -6,16 +6,32 @@ $(document).ready(function()    {
   $('#masthead-photo').delay(200).animate({opacity:1.0}, 'linear', function(){ 
     $('#windows8').animate({opacity:0, 'margin-left':'-70px'}, 'linear');
     $('#masthead-photo-extension').animate({opacity:1.0, 'margin-left':'0em'}, 'linear', function(){
-      $('#brand-logo-ul').delay(400).animate({opacity:1.0}, 'linear');
+      $('#brand-logo-li-1').delay().animate({opacity:1.0}, 'linear', function(){       
+        $('#brand-logo-li-2').delay().animate({opacity:1.0}, 'linear', function(){ 
+          $('#brand-logo-li-3').delay().animate({opacity:1.0}, 'linear', function(){ 
+            $('#brand-logo-li-4').delay().animate({opacity:1.0}, 'linear', function(){ 
+              $('#brand-logo-li-5').delay().animate({opacity:1.0}, 'linear', function(){ 
+                $('#brand-logo-li-6').delay().animate({opacity:1.0}, 'linear', function(){
+                  $('.brand-logo-ul').delay(5400).animate({opacity:1.0}, 'linear',
+                  function(){
+                    $('#brand-logo-case').addClass("active-now");
+                    // Places a containing css selector to make sure the second round of slides all come in opacity:1
+                  });
+                });
+              });
+            });
+          });
+        });
+      });
     });
   });
   $('#featsucc').foundation('orbit');
   $('#brandslider').foundation('orbit',{
-      timer_speed: 8000
+      timer_speed: 13000
     , stack_on_small: false
     , animation_speed: 1000
     , bullets: false
-  });
+  }, function(response){});
   $('#pausetarget').find('.orbit-timer span').click();
   
 });
