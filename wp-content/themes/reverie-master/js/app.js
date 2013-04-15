c289d2393212f88adc478c78f0d331a45f6e7fa0
lkbgift <lkbgift@gmail.com>
2013-04-15 11:04:07 -0400

    refactored delay on logos

--- wp-content/themes/reverie-master/js/app.js
+++ wp-content/themes/reverie-master/js/app.js
@@ -12,21 +12,16 @@ $(document).ready(function()    {
     $('#masthead-photo-extension').animate({opacity:1.0, 'margin-left':'0em'}, 'linear', function(){
 
       // Stagger the brand appearance for first set
-      $('#brand-logo-li-1').delay().animate({opacity:1.0}, 'linear', function(){       
-        $('#brand-logo-li-2').delay().animate({opacity:1.0}, 'linear', function(){ 
-          $('#brand-logo-li-3').delay().animate({opacity:1.0}, 'linear', function(){ 
-            $('#brand-logo-li-4').delay().animate({opacity:1.0}, 'linear', function(){ 
-              $('#brand-logo-li-5').delay().animate({opacity:1.0}, 'linear', function(){ 
-                $('#brand-logo-li-6').delay().animate({opacity:1.0}, 'linear', function(){
-                  $('.brand-logo-ul').delay(4000).animate({opacity:1.0}, 'linear',
-                  function(){
-                    // Places a containing css selector to make sure the second round of slides all come in opacity:1
-                    $('#brand-logo-case').addClass("active-now");
-                  });
-                });
-              });
-            });
-          });
+      $('#brand-logo-li-1').delay().animate({opacity:1.0});       
+      $('#brand-logo-li-2').delay(45).animate({opacity:1.0});
+      $('#brand-logo-li-3').delay(70).animate({opacity:1.0});
+      $('#brand-logo-li-4').delay(85).animate({opacity:1.0});
+      $('#brand-logo-li-5').delay(90).animate({opacity:1.0});
+      $('#brand-logo-li-6').delay(93).animate({opacity:1.0}, 'linear', function(){
+      $('.brand-logo-ul').delay(4000).animate({opacity:1.0}, 'linear',
+        function(){
+          // Places a containing css selector to make sure the second round of slides all come in opacity:1
+          $('#brand-logo-case').addClass("active-now");
         });
       });
     });
