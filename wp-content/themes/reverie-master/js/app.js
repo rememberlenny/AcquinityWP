04b4a941ced8e80d7c8033c672a11a10381ca344
lkbgift <lkbgift@gmail.com>
2013-04-15 11:37:43 -0400

    fixing up load

--- wp-content/themes/reverie-master/js/app.js
+++ wp-content/themes/reverie-master/js/app.js
@@ -17,12 +17,11 @@ $(document).ready(function()    {
       $('#brand-logo-li-3').delay(80).animate({opacity:1.0});
       $('#brand-logo-li-4').delay(95).animate({opacity:1.0});
       $('#brand-logo-li-5').delay(100).animate({opacity:1.0});
-      $('#brand-logo-li-6').delay(103).animate({opacity:1.0}, 'linear', function(){
+      $('#brand-logo-li-6').delay(103).animate({opacity:1.0});
       $('.brand-logo-ul').delay(105).animate({opacity:1.0}, 'linear',
-        function(){
-          // Places a containing css selector to make sure the second round of slides all come in opacity:1
-          $('#brand-logo-case').addClass("active-now");
-        });
+      function(){
+        // Places a containing css selector to make sure the second round of slides all come in opacity:1
+        $('#brand-logo-case').addClass("active-now");
       });
     });
   });
