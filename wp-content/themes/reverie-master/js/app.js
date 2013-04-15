4f66a43b7da34c42324d266693bb650a460c089a
lkbgift <lkbgift@gmail.com>
2013-04-15 11:18:24 -0400

    adding slider

--- wp-content/themes/reverie-master/js/app.js
+++ wp-content/themes/reverie-master/js/app.js
@@ -14,11 +14,11 @@ $(document).ready(function()    {
       // Stagger the brand appearance for first set
       $('#brand-logo-li-1').delay().animate({opacity:1.0});       
       $('#brand-logo-li-2').delay(45).animate({opacity:1.0});
-      $('#brand-logo-li-3').delay(70).animate({opacity:1.0});
-      $('#brand-logo-li-4').delay(85).animate({opacity:1.0});
-      $('#brand-logo-li-5').delay(90).animate({opacity:1.0});
-      $('#brand-logo-li-6').delay(93).animate({opacity:1.0}, 'linear', function(){
-      $('.brand-logo-ul').delay(4000).animate({opacity:1.0}, 'linear',
+      $('#brand-logo-li-3').delay(80).animate({opacity:1.0});
+      $('#brand-logo-li-4').delay(95).animate({opacity:1.0});
+      $('#brand-logo-li-5').delay(100).animate({opacity:1.0});
+      $('#brand-logo-li-6').delay(103).animate({opacity:1.0}, 'linear', function(){
+      $('.brand-logo-ul').delay(105).animate({opacity:1.0}, 'linear',
         function(){
           // Places a containing css selector to make sure the second round of slides all come in opacity:1
           $('#brand-logo-case').addClass("active-now");
