273ff169d241abbeee76350dfc71e339c5ece196
lkbgift <lkbgift@gmail.com>
2013-04-17 14:57:21 -0400

    added custom post thumnail function

--- wp-content/themes/reverie-master/content-archive.php
+++ wp-content/themes/reverie-master/content-archive.php
@@ -12,9 +12,23 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
   <div class="large-4 column">
   	<div class="thumb-wrapper circlewrapper">
-  	<?php if (has_post_thumbnail( $post->ID ) ): ?>
-      <a href="<?php the_permalink(); ?>"><?php $image = wp_get_attachment_image_src( the_post_thumbnail( 'thumbnail' ), 'single-post-thumbnail' ); ?></a>
-    <?php endif; ?>
+      <?php 
+
+            
+      if (has_post_thumbnail( $post->ID ) ): ?>
+    <a href="<?php the_permalink(); ?>"><?php 
+      // Check if post has a Post Custom Thumbnail set
+      if( get_field('toggle_post_custom_thumbnaill') ):?>
+        <img src="<?php the_field('image_post_custom_thumbnail'); ?>" alt="" />
+        <?php
+      // Else display normal post thumbnail
+      else:
+      $image = wp_get_attachment_image_src( the_post_thumbnail( 'thumbnail' ), 'single-post-thumbnail' ); 
+        endif;
+    ?></a>
+      <?php 
+      endif; 
+      ?>
     </div>
   </div>
   <div class="content-block large-8 column"> 
