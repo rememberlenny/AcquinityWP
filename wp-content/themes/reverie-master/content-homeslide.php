a16b0c2c645e47be0976285450f427f11022c207
lkbgift <lkbgift@gmail.com>
2013-04-26 14:16:30 -0400

    saving changes to states and side bar

--- wp-content/themes/reverie-master/content-homeslide.php
+++ wp-content/themes/reverie-master/content-homeslide.php
@@ -6,31 +6,31 @@
  * @since Reverie 4.0
  */
 ?>
-	
+<style>
+  .orbit-next, .orbit-prev, .orbit-slide-number, .orbit-timer{ display:none; }
+
+</style>	
  <li class="active text-left article-title" data-orbit-slide="headline-1">
    <div id="post-<?php the_ID(); ?>">
-      <div class="large-3 small-3 mkup column mn93">
-        <div class="hide-for-small image-wrapper circlewrapper radius" >
-          <?php if (has_post_thumbnail( $post->ID ) ): ?>
-            <a href="<?php the_permalink(); ?>"><?php $image = wp_get_attachment_image_src( the_post_thumbnail( 'thumbnail' ), 'single-post-thumbnail' ); ?></a>
-          <?php endif; ?>
-        </div>
-      </div>
-      <div class="large-9 small-12 column pl98">
+      <div class="large-12 small-12 column pl98">
        <header>
-        <h5>
+        <div class="row">
+        <h5 style="margin-top:0em;">
           <a href="<?php the_permalink(); ?>" data-orbit-slide="headline-2" class="secondary">
             <?php the_title(); ?>
           </a>
         </h5> 
+        </div>
     	</header>
-    		<p>
+    		<div class="row">
+        <p>
           <?php
             $excerpt = get_the_excerpt();
             echo string_limit_words($excerpt,25);
           ?>... 
           <a href="<?php the_permalink(); ?>">Read More ›</a>
         </p>  
+        </div>
     	<footer>
     		<?php $tag = get_the_tags(); if (!$tag) { } else { ?><p><?php the_tags(); ?></p><?php } ?>
     	</footer>
--- wp-content/themes/reverie-master/page-home.php
+++ wp-content/themes/reverie-master/page-home.php
@@ -21,20 +21,89 @@ get_header(); ?>
 <?php brandBarHome(); ?>
 </div>
 	
+<style>
+  .stats h4{
+    color: #333 !important;
+    margin-bottom:0em;
+  }
+  .stats h5{
+    color: #333 !important;
+    margin-top:0em;
+  }
+  .orbit-bullets{ display: none;}
+  .orbit-container{ background: none; }
+</style>
+
 <div class="content content-main row twelve">
 	<div class="main large-8 columns "> 	
 		<div class="case-studies fr">
 		  <div class="s9999 page-main-extension whitebg fr">
         <div class="lftcont page-main-content content-sec fr">
-          <div class="pr98 pl98 text-left column"> 
+          <div class="pr98 pl98 text-left column stats"> 
             <div class=" column">
-              <a href="/success-story/">
-                <h4 class="subheader">Success Stories</h4>
-              </a>
-
-              <div class="case-study-item text-left large-12 fl">
-               <div class="featsucc" id="pausetarget"  > 
-                  <ul id="featsucc" data-orbit data-options="timer_speed: 0;">
+              <div class="row">
+                <div class="large-4 column">
+                  <h4 class="subheader">
+                    Email
+                  </h4>
+                  <div class="textwidget">
+                    <div class="d-inline">
+                      <h5 class="subheader white stat-number">
+                        80MM
+                      </h5>
+                      <p>
+                        in-house database
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div class="large-4 column">
+                  <h4 class="subheader">
+                    Websites
+                  </h4>
+                  <div class="textwidget">
+                    <div class="d-inline">
+                      <h5 class="subheader white stat-number">
+                        20MM
+                      </h5>
+                      <p>
+                        monthly pageviews
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div class="large-4 column">
+                  <h4 class="subheader">
+                    Call Centers
+                  </h4>
+                  <div class="textwidget">
+                    <div class="d-inline">
+                      <h5 class="subheader white stat-number">
+                        2.6MM
+                      </h5>
+                      <p>
+                        leads qualified monthly
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            <div class=" clear text"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+		</div>	
+		<div class="page-main fr text-left" role="main">
+	    <div class="s9999 page-main-extension newsynew fr">
+	      <div class="lftcont page-main-content content-sec fr pb15em">
+		      <div class="pr98 pl98 text-left column"> 
+            <div class=" ">
+              <div class="large-6 column featart-short borderrt">
+                <a href="/success-story/">
+                  <h4 class="subheader">Recent Success Stories</h4>
+                </a>
+                <ul id="featsucc" data-orbit data-options="timer_speed: 0;">
                     <?php 
                     rewind_posts();
                     $mypost = array( 'post_type' => 'success-story' );
@@ -52,41 +121,20 @@ get_header(); ?>
 
                     <?php endif; // end have_posts() check 
                     wp_reset_query();?>
-                  </ul>
-                </div>
-              </div>
-            <div class=" clear text"></div>
-            </div>
-          </div>
-        </div>
-      </div>
-		</div>	
-		<div class="page-main fr text-left" role="main">
-	    <div class="s9999 page-main-extension newsynew fr">
-	      <div class="lftcont page-main-content content-sec fr pb15em">
-		      <div class="pr98 pl98 text-left column"> 
-            <div class=" ">
-              <div class="large-6 column featart-short borderrt">
+                  </ul>  
+               </div>
+               <div class="text-left large-6 column">
                 <a href="/news/">
-                  <h4 class="subheader">Latest</h4>
+                  <h4 class="subheader">Acquinity News</h4>
                 </a>
+                <ul class="" style="margin-bottom: 1.8em;">
                   <?php 
                   rewind_posts();
                   $mypost = array( 'post_type' => 'post' );
                   $my_query = new WP_Query( $mypost ); ?>
 
                   <?php if ( have_posts() ) : ?>
-
                   <?php $i=1 /* Start the Loop */ ?>
-                  <?php while ( $i<2 && $my_query->have_posts()) :  $my_query->the_post(); ?>
-                  <?php get_template_part( 'content', 'newpost' ); ?> 
-                  <?php $i++; endwhile; ?>
-               </div>
-               <div class="text-left large-6 column">
-                <a href="/news/">
-                  <h4 class="subheader">News</h4>
-                </a>
-                <ul class="" style="margin-bottom: 1.8em;">
                   <?php while ( $i<5 && $my_query->have_posts()) :  $my_query->the_post(); ?>
                   <?php get_template_part( 'content', 'notnewpost' ); ?>
                   <?php $i++; endwhile; ?>
@@ -103,24 +151,7 @@ get_header(); ?>
 		    </div>
 		  </div>
 		</div>
-    <div class="page-main fr text-left" role="main">
-      <div class="s9999 page-main-extension whitebg borderbtm fr ">
-        <div class="lftcont page-main-content content-sec fr">
-          <div class="pr98 pl98 text-left column"> 
-            <div class="column">
-              <header>
-                <h4 class="subheader"><?php the_field('home_section2_title'); ?></h4>
-              </header>
-              <div class="entry-content">
-                <?php the_field('product_section'); ?>
-              </div>
-            </div>
-            <div class=" clear text">
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>  
+    
 	</div>
 	<?php get_sidebar(); ?>
 </div>
