172e93c75b202c2b3d62a38def4a2e17f6758b20
lkbgift <lkbgift@gmail.com>
2013-05-17 14:56:25 -0400

    options media kit

--- wp-content/themes/reverie-master/includes/lkbg_options_page.php
+++ wp-content/themes/reverie-master/includes/lkbg_options_page.php
@@ -5,6 +5,7 @@ if(function_exists("register_options_page"))
     register_options_page('News and Success Stories');
     register_options_page('Sidebar');
     register_options_page('Social Media and Logo');
+    register_options_page('Media Kit');
  }
 
 ?>
