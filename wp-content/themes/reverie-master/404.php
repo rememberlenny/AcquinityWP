dae85e90e765493edc0444cb02cb8654cf1be1a8
lkbgift <lkbgift@gmail.com>
2013-05-17 12:03:49 -0400

    404

--- wp-content/themes/reverie-master/404.php
+++ wp-content/themes/reverie-master/404.php
@@ -1,26 +1,44 @@
 <?php get_header(); ?>
 
-<!-- Row for main content area -->
-	<div class="small-12 large-8 columns" role="main">
-	
-		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
-			<header>
-				<h1 class="entry-title"><?php _e('File Not Found', 'reverie'); ?></h1>
-			</header>
-			<div class="entry-content">
-				<div class="error">
-					<p class="bottom"><?php _e('The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.', 'reverie'); ?></p>
-				</div>
-				<p><?php _e('Please try the following:', 'reverie'); ?></p>
-				<ul> 
-					<li><?php _e('Check your spelling', 'reverie'); ?></li>
-					<li><?php printf(__('Return to the <a href="%s">home page</a>', 'reverie'), home_url()); ?></li>
-					<li><?php _e('Click the <a href="javascript:history.back()">Back</a> button', 'reverie'); ?></li>
-				</ul>
+<div class="content content-main row">
+	<div class="main large-9 columns "> 	
+		<?php /* Start loop */ ?>
+		<div class="case-studies fr">
+		  <div class="s9999 case-studies-extension fr">
+		    <div id="case-sliders" class="pr98 pl98 lftcont case-studies-content content-sec fr">
+	      		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
+							<header>
+								<h1 class="entry-title"><?php _e('File Not Found', 'reverie'); ?></h1>
+							</header>
+							<div class="entry-content">
+								<div class="error">
+									<p class="bottom"><?php _e('The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.', 'reverie'); ?></p>
+								</div>
+								<p><?php _e('Please try the following:', 'reverie'); ?></p>
+								<ul> 
+									<li><?php _e('Check your spelling', 'reverie'); ?></li>
+									<li><?php printf(__('Return to the <a href="%s">home page</a>', 'reverie'), home_url()); ?></li>
+									<li><?php _e('Click the <a href="javascript:history.back()">Back</a> button', 'reverie'); ?></li>
+								</ul>
+							</div>
+						</article>
+			  </div>
 			</div>
-		</article>
-
+		</div>
 	</div>
-	<?php get_sidebar(); ?>
-		
+
+<?php get_template_part( 'content-sidebar'); ?>
+	
+</div>
+
+<?php get_template_part( 'content-emailbar'); ?>
+
+<?php get_template_part( 'content-successstories'); ?>
+
 <?php get_footer(); ?>
+
+
+	
+
+
+	
